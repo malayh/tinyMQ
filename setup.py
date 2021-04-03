761ce6ad40680754c574a7e7491804c7443a7e68
Malay <malay.hazarika@gmail.com>
2021-04-03 21:39:35 +0530

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt","r") as f:
+    dependencies = f.read().splitlines()
+
+setuptools.setup(
+    name="tinyMQ",
+    version="0.0.1",
+    author="Malay Hazarika",
+    author_email="malay.hazarika@gmail.com",
+    description="Transient inmemory Messaging Queue",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/malayh/tinyMQ",
+    project_urls={
+        "Bug Tracker": "https://github.com/malayh/tinyMQ/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=["tinyMQ"],
+    python_requires=">=3.6",
+    install_requires=dependencies,
+    
+    entry_points={
+        'console_scripts': [
+            'tmqserver = tinyMQ:execute_cli'
+        ]
+    },
+    
+)
